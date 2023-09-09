1f886a32f9834489b27204bc64d5ab34d716acc2
PercyGFX <isurangabtk@gmail.com>
2023-09-09 13:32:10 +0530

    add petient move to new class

---
+++ HealthCare Plus/HealthCare Plus/Controllers/patientsClass.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare_Plus.Controllers
+{
+
+    internal class patientsClass
+    {
+        // database object 
+        Database dbManager = new Database();
+
+
+        //////////// add patient //////////////////
+        ///
+
+        public bool InsertPatientData(
+        string name,
+        string age,
+        string address,
+        string phone,
+        string bloodType,
+        string description,
+        string isActive)
+        {
+
+            using (MySqlConnection connection = dbManager.GetConnection())
+            {
+                dbManager.OpenConnection(connection);
+
+                string insertQuery = "INSERT INTO patient (name, age, address, phone, blood_type, description, is_active) VALUES (@Name, @Age, @Address, @Phone, @BloodType, @Description, @IsActive)";
+
+                using (MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("@Name", name);
+                    insertCommand.Parameters.AddWithValue("@Age", age);
+                    insertCommand.Parameters.AddWithValue("@Address", address);
+                    insertCommand.Parameters.AddWithValue("@Phone", phone);
+                    insertCommand.Parameters.AddWithValue("@BloodType", bloodType);
+                    insertCommand.Parameters.AddWithValue("@Description", description);
+                    insertCommand.Parameters.AddWithValue("@IsActive", isActive);
+
+                    int rowsAffected = insertCommand.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+
+
+    }
+}
--- HealthCare Plus/HealthCare Plus/views/admin/addpatient.cs
+++ HealthCare Plus/HealthCare Plus/views/admin/addpatient.cs
@@ -1,4 +1,5 @@
-﻿using HealthCare_Plus.Services;
+﻿using HealthCare_Plus.Controllers;
+using HealthCare_Plus.Services;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
@@ -14,8 +15,9 @@ namespace HealthCare_Plus.views.admin
 {
     public partial class addpatient : UserControl
     {
-        // database object 
-        Database dbManager = new Database();
+
+        patientsClass patients = new patientsClass();
+
 
         public addpatient()
         {
@@ -25,12 +27,12 @@ namespace HealthCare_Plus.views.admin
         private void btnadd_Click(object sender, EventArgs e)
         {
             if (AddPatientValidations.IsValidFormFields(
-                 txtname.Text.Trim(),
-                 txtage.Text.Trim(),
-                 txtaddress.Text.Trim(),
-                 txtphone.Text.Trim(),
-                 txtblood.Text.Trim(),
-                 rtextdescription.Text.Trim()))
+            txtname.Text.Trim(),
+             txtage.Text.Trim(),
+                txtaddress.Text.Trim(),
+                txtphone.Text.Trim(),
+                txtblood.Text.Trim(),
+                rtextdescription.Text.Trim()))
             {
                 string name = txtname.Text.Trim();
                 string age = txtage.Text.Trim();
@@ -40,41 +42,23 @@ namespace HealthCare_Plus.views.admin
                 string description = rtextdescription.Text.Trim();
                 string isActive = "Active"; // Hardcoded "Active"
 
-                using (MySqlConnection connection = dbManager.GetConnection())
+                if (patients.InsertPatientData(name, age, address, phone, bloodType, description, isActive))
                 {
-                    dbManager.OpenConnection(connection);
+                    MessageBox.Show("Patient data added successfully.", "Success");
 
-                    // Create and execute the database query to insert the data
-                    string insertQuery = "INSERT INTO patient (name, age, address, phone, blood_type, description, is_active) VALUES (@Name, @Age, @Address, @Phone, @BloodType, @Description, @IsActive)";
-                    using (MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection))
+                    // Load back to the main form (adjust form name accordingly)
+                    patients patients = new patients();
+                    if (ParentForm is adminDashboard adminDashboard)
                     {
-                        insertCommand.Parameters.AddWithValue("@Name", name);
-                        insertCommand.Parameters.AddWithValue("@Age", age);
-                        insertCommand.Parameters.AddWithValue("@Address", address);
-                        insertCommand.Parameters.AddWithValue("@Phone", phone);
-                        insertCommand.Parameters.AddWithValue("@BloodType", bloodType);
-                        insertCommand.Parameters.AddWithValue("@Description", description);
-                        insertCommand.Parameters.AddWithValue("@IsActive", isActive);
-
-                        int rowsAffected = insertCommand.ExecuteNonQuery();
-                        if (rowsAffected > 0)
-                        {
-                            MessageBox.Show("Patient data added successfully.", "Success");
-
-                            // Load back to the main form (adjust form name accordingly)
-                            patients patients = new patients();
-                            if (ParentForm is adminDashboard adminDashboard)
-                            {
-                                adminDashboard.loadform(patients);
-                            }
-                        }
-                        else
-                        {
-                            MessageBox.Show("Failed to add patient data.", "Error");
-                        }
+                        adminDashboard.loadform(patients);
                     }
                 }
+                else
+                {
+                    MessageBox.Show("Failed to add patient data.", "Error");
+                }
             }
+
         }
     }
 }
